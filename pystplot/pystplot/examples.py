e214d66ced29f3a65c0ca6f3b185b26ea63b79a6
Gustaf Sterner <gsterner@gmail.com>
2016-05-06 17:59:43 +0200

    Changing subplot examples

--- pystplot/pystplot/examples.py
+++ pystplot/pystplot/examples.py
@@ -7,19 +7,19 @@ y_vals = [math.exp(-x*x) for x in x_vals]
 sin_vals =  [math.sin(x) for x in x_vals]
 x2 = [2*x for x in x_vals]
 
-pl.subplot(4,1,1)
+pl.subplot(2,2,1)
 pl.plot(x_vals, y_vals, '-og')
 pl.plot(x_vals, x_vals, '-o')
 
-pl.subplot(4,1,2)
+pl.subplot(2,2,2)
 pl.plot(x_vals, x2,'-r')
 pl.plot(x_vals, sin_vals,'ro')
 
-pl.subplot(4,1,3)
+pl.subplot(2,2,3)
 pl.plot(x_vals, x2,'-r')
 pl.plot(x_vals, sin_vals,'ro')
 
-pl.subplot(4,1,4)
+pl.subplot(2,2,4)
 pl.plot(x_vals, y_vals, '-o')
 pl.plot(x_vals, x_vals, '-o')
 
