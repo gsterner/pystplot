eaeca5f8007bd1d3ebdf391bdebe9f76bf560264
Gustaf Sterner <gsterner@gmail.com>
2016-05-07 19:22:41 +0200

    Fixed typo in setup.py

--- pystplot/setup.py
+++ pystplot/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pystplot',
       version='0.1',
       description='Browser based plotting in python',
-      url='http://github.com/gsterner/sternplot-python'
+      url='http://github.com/gsterner/sternplot-python',
       author='Gustaf Sterner',
       author_email='gsterner@gmail.com',
       license='MIT',
